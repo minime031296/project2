0833d6df68b73e0eaa6999bfcae3bb2b8a13f360
minime031296 <140069366+minime031296@users.noreply.github.com>
2023-07-20 22:04:06 +0530

    Update max.py

--- project 2/max.py
+++ project 2/max.py
@@ -4,11 +4,14 @@ t= turtle.Turtle()
 turtle.bgcolor("cyan")
 for x in range(50):
     t.pencolor(colors[x%6])
-    t.fd(x*10)   
-    t.right(144) 
+    t.fd(x*20)   
+    t.right(90.1) 
+    t.lt(90)
+    t.rt(90.1)
     t.color("yellow")
     t.begin_fill()
-    for i in range(5):
-        t.fd(10)
-        t.rt(1)
+    # for i in range(5):
+    #     t.fd(10)
+    #     t.rt(1)
     t.end_fill()
+
