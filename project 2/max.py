9db0489cdb7f3e3da26bc6bed9d7426c7f6fcd7e
minime031296 <140069366+minime031296@users.noreply.github.com>
2023-07-22 19:23:10 +0530

    Update max.py

--- project 2/max.py
+++ project 2/max.py
@@ -1,17 +1,12 @@
 import turtle
 colors=["red","purple","blue","green","orange","yellow","pink"]
-t= turtle.Turtle()
-t.begin_fill
+jeff= turtle.Turtle()
+jeff.begin_fill
 turtle.bgcolor("white")
-for x in range(50):
-    t.pencolor(colors[x%7])
-    t.fd(x*20)   
-    t.right(90.1) 
-    t.fd(x+50)
-    t.rt(10)
-    t.lt(190)
-    t.fillcolor("yellow")
-    t.fd(x*10)
-    t.rt(160)
-    t.color("yellow")
-t.end_fill()
+for x in range(15):
+    jeff.pencolor(colors[x%6])
+    jeff.circle(100)
+    jeff.fd(1)
+    jeff.rt(1600)
+    jeff.rt(100)
+
